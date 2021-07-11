acf9d73dc8bcbdead676144da96901c5e6417991
Aleh Dumava <odumava@mail.ru>
2021-07-11 19:33:35 +0300

    Task 1 - New Year Present

---
+++ MyFirstProject/src/domain/Candy.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Candy extends Sweets {
+	
+	private String typeCandy;
+	private String titleCandy;
+	private Integer weightCandy;
+	
+
+}
---
+++ MyFirstProject/src/domain/Sweets.java
@@ -0,0 +1,21 @@
+package domain;
+
+public class Sweets {
+	
+	private String	typeSweet;
+	private String	titleSweet;
+	private Integer	weightSweet;
+	
+	public String getTypeSweet() {
+		return typeSweet;
+	}
+	
+	public String getTitleSweet() {
+		return titleSweet;
+	}
+	
+	public Integer getWeightSweet() {
+		return weightSweet;
+	}
+
+}
