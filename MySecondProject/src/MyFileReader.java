a57639e02d3adbe27071e8a457b272911a766e6a
Aleh Dumava <odumava@mail.ru>
2021-08-05 23:05:55 +0300

    Task 2 - Changed reading the files.

--- MySecondProject/src/MyFileReader.java
+++ MySecondProject/src/MyFileReader.java
@@ -1,11 +1,17 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MyFileReader {
 
 	public static void main(String[] args) throws IOException {
 		
 		System.out.println("Start copy file");
-		String path = "c:/Users/User/eclipse-workspace/MySecondProject/src/";
+		String path = "c://Users//User//git//training//MySecondProject//";
 		String getString;
 		
 		try {
@@ -15,6 +21,7 @@ public class MyFileReader {
 				int countString = 1;
 				
 				while((getString = input.readLine()) != null) {
+					System.out.println(getString);
 					output.write("["+countString+"]" + getString.toLowerCase());
 					output.newLine();
 					countString++;
@@ -24,7 +31,8 @@ public class MyFileReader {
 				output.close();
 			}
 			catch(FileNotFoundException e) {
-				System.out.println("File not found!" + e);
+				System.out.println("The file was not found!" + e);
+				System.exit(0);
 			}
 		System.out.println("The file is copyed.");
 	}
