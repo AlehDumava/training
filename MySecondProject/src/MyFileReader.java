958d19a8f74b66ff5f945a1ca8f6f514677c1592
Aleh Dumava <odumava@mail.ru>
2021-08-08 18:48:21 +0300

    Task 2 - Add new Class BreakIterator.

--- MySecondProject/src/MyFileReader.java
+++ MySecondProject/src/MyFileReader.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
+import java.text.BreakIterator;
+import java.util.Locale;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,24 +9,55 @@ import java.io.IOException;
 
 public class MyFileReader {
 
+
+
 	public static void main(String[] args) throws IOException {
 		
 		System.out.println("Start copy file");
 		String path = "c://Users//User//git//training//MySecondProject//";
 		String getString;
-		
+				
 		try {
 				BufferedReader input = new BufferedReader(new FileReader(path+"MyTextFile.txt"));
 				BufferedWriter output = new BufferedWriter(new FileWriter(path+"MyNewTextFile.txt"));
 				
-				int countString = 1;
+				BreakIterator splitIntoSentences = BreakIterator.getSentenceInstance();
+				BreakIterator wordIterator = BreakIterator.getWordInstance();
+				
 				
 				while((getString = input.readLine()) != null) {
-					System.out.println(getString);
-					output.write("["+countString+"]" + getString.toLowerCase());
-					output.newLine();
-					countString++;
+									
+					splitIntoSentences.setText(getString);
+					int index = 0;
+					
+					while (splitIntoSentences.next() != BreakIterator.DONE) {
+						
+				        String sentence = getString.substring(index, splitIntoSentences.current());
+				        index = splitIntoSentences.current();
+				        System.out.print(sentence);
+				        
+				        	wordIterator.setText(sentence);
+				        	int start = wordIterator.first();
+				        	int end = wordIterator.next();
+				        	int counterWords = 0;
+				        
+				        	while (end != BreakIterator.DONE) {
+				        	
+				        		String word = sentence.substring(start, end);
+				        		if (Character.isLetterOrDigit(word.charAt(0))) {
+				        			counterWords++;	        	    
+				        		}
+				        		start = end;
+				        		end = wordIterator.next();
+				        		
+				            }
+				        	System.out.println("	Count words: " + counterWords);
+				      }
+					
 				}
+				System.out.println("+++++++++++++++++++++++++");
+				
+						
 				
 				input.close();
 				output.close();
