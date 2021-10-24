49302f598bcc5a2bda0a74605bfea80aa00bcd02
Aleh Dumava <odumava@mail.ru>
2021-10-24 19:26:37 +0300

    Task 4,5 - Update class Grades

--- MyFinalProject/src/main/java/beans/Grades.java
+++ MyFinalProject/src/main/java/beans/Grades.java
@@ -2,39 +2,39 @@ package beans;
 
 public class Grades {
 
-	public Integer userID;
-	public float subjectFirst;
-	public float subjectSecond;
-	public float subjectThird;
-	public float sertificateScore;
-	public float totalScore;
+	public Integer id;
+	public float subject_first;
+	public float subject_second;
+	public float subject_third;
+	public float sertificate_score;
+	public float id_users;
 	
 	public Grades() {
 		super();
 	}
 
-	public void setUserID(Integer userID) {
-		this.userID = userID;
+	public void setUserID(Integer id) {
+		this.id = id;
 	}
 
-	public void setSubjectFirst(float subjectFirst) {
-		this.subjectFirst = subjectFirst;
+	public void setSubjectFirst(float subject_first) {
+		this.subject_first = subject_first;
 	}
 
-	public void setSubjectSecond(float subjectSecond) {
-		this.subjectSecond = subjectSecond;
+	public void setSubjectSecond(float subject_second) {
+		this.subject_second = subject_second;
 	}
 
-	public void setSubjectThird(float subjectThird) {
-		this.subjectThird = subjectThird;
+	public void setSubjectThird(float subject_third) {
+		this.subject_third = subject_third;
 	}
 
-	public void setSertificateScore(float sertificateScore) {
-		this.sertificateScore = sertificateScore;
+	public void setSertificateScore(float sertificate_score) {
+		this.sertificate_score = sertificate_score;
 	}
 
-	public void setTotalScore(float totalScore) {
-		this.totalScore = totalScore;
+	public void setTotalScore(float id_users) {
+		this.id_users = id_users;
 	}
 	
 	
